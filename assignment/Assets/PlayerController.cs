b457e03e2787e7e2b85e908573ac4f85ef32ace9
pin410 <seoyeonpin410@gmail.com>
2026-03-24 23:36:02 +0900

    Add:플레이어 기본 움직임

--- assignment/Assets/PlayerController.cs
+++ assignment/Assets/PlayerController.cs
@@ -5,6 +5,9 @@ public class PlayerController : MonoBehaviour
 {
     private Vector2 moveInput;
     public float moveSpeed = 7f;
+    public float jumpForce = 7f;
+    private Rigidbody2D rb;
+    private Animator myAnimator;
 
     public void OnMove(InputValue value)
     {
@@ -13,7 +16,17 @@ public class PlayerController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myAnimator.SetBool("move", false);
+    }
+
+    public void OnJump(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
     }
 
     // Update is called once per frame
@@ -23,10 +36,18 @@ public class PlayerController : MonoBehaviour
         {
             transform.localScale = new Vector3(1, 1, 1);
         }
-        else if (moveInput.x > 0)
+        else if (moveInput.x < 0)
         {
             transform.localScale = new Vector3(-1, 1, 1);
         }
-        transform.Translate(Vector3.right * moveSpeed * moveInput.x * Time.deltaTime);
+        if(moveInput.magnitude > 0)
+        {
+            myAnimator.SetBool("move", true);
+        }
+        else
+        {
+            myAnimator.SetBool("move", false);
+        }
+            transform.Translate(Vector3.right * moveSpeed * moveInput.x * Time.deltaTime);
     }
 }
